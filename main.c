8a58f8f645a53bbeffeed387a4695da31fc828dd
d0pelrh <jsshscifi@gmail.com>
2023-12-01 20:50:54 +0100

    display windows in the right order

--- main.c
+++ main.c
@@ -1,22 +1,33 @@
 #include <ncurses.h>
 #include <stdlib.h>
+#include <time.h>
 //#include "whispers.h"
 #define GWH 30
 #define GWW 80
 #define K_QUIT '0'
 #define K_INV 'i'
+#define WHISP_INTERVAL 10
+#define WHISP_DURATION 5
+struct {
+	struct Chunk* left;
+	struct Chunk* right;
+	struct Chunk* top;
+	struct Chunk* bottom;
+	char** buffer;
+} Chunk;
 
 int main(int ac, char** av){
+srand(time(NULL));
 initscr();
   cbreak(); noecho();
   curs_set(0); start_color();
+  refresh();
 
   int gamewinh =LINES>GWH? GWH: LINES,
   gamewinw =LINES>GWW? GWW: COLS,
   gamewiny =(LINES-gamewinh)/2,
   gamewinx =(COLS-gamewinw)/2;
 WINDOW* gamewin =newwin(gamewinh,gamewinw,gamewiny,gamewinx);
-box(gamewin,0,0);
   int talkmarginleft =8,
   talkmarginbottom =2,
   talkwinh =5,
@@ -33,20 +44,25 @@ WINDOW* talkwin =newwin(talkwinh,talkwinw,talkwiny,talkwinx);
 WINDOW* invwin =newwin(invwinh,invwinw,invwiny,invwinx);
 int inv_on =0;
 
-char c =0;
-do {
+char c =0,count =0;
+do { count++;
 
 switch(c){
-case K_INV: if(!inv_on) inv_on =1;
-	else { inv_on =0; werase(invwin); wrefresh(invwin); } break;
-default: break;}
+case K_INV:	if(!inv_on) inv_on =1;
+		else { inv_on =0; werase(invwin); wrefresh(invwin); } break;
+	default: break;}
 
-
+if(count==WHISP_DURATION){
+werase(talkwin); wrefresh(talkwin); count =0;}
 box(gamewin,0,0);
 wrefresh(gamewin);
 if(inv_on){
-	box(invwin,0,0);
-	wrefresh(invwin);}
+box(invwin,0,0);
+mvwprintw(invwin,1,2,"INVENTORY");
+wrefresh(invwin);}
+if(count<=WHISP_INTERVAL){
+box(talkwin,0,0);
+wrefresh(talkwin);}
 
 } while((c=getch())!=K_QUIT);
 
